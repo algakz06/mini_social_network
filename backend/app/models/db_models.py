758a29e08d71387285d07bab3aa73f3c22ac126a
algakz06 <77662392+algakz06@users.noreply.github.com>
2023-07-10 21:27:58 +0300

    models for db was written

--- backend/app/models/db_models.py
+++ backend/app/models/db_models.py
@@ -1,6 +1,38 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Numeric, Index
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from datetime import datetime
+import enum
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    email = Column(String(50), nullable=False)
+    hashed_password = Column(String(50), nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False)
+    content = Column(String(500), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    created_at = Column(DateTime, default=datetime.now())
+
+
+class ReactionType(enum.Enum):
+    LIKE = "like"
+    DISLIKE = "dislike"
+
+
+class Reaction(Base):
+    __tablename__ = "reaction"
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(ReactionType), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    post_id = Column(Integer, ForeignKey("post.id"))
+    created_at = Column(DateTime, default=datetime.now())
