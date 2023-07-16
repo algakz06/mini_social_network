d7b1d50eec32b66791de34a1dedde69e5d1de94e
algakz06 <77662392+algakz06@users.noreply.github.com>
2023-07-16 18:32:56 +0300

    docker launching was added for encoding link task

--- encode_link/main.py
+++ encode_link/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
 from urllib.parse import quote
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
-@app.get("/encode_url/{url}")
-def encode_url(url: str):
-    encoded_url = quote(url)
+class EncodeUrl(BaseModel):
+    url: str
+
+
+@app.post("/encode_url")
+def encode_url(url_for_encode: EncodeUrl):
+    encoded_url = quote(url_for_encode.url)
     return {"encoded_url": encoded_url}
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(app, host="0.0.0.0", port=2323)
